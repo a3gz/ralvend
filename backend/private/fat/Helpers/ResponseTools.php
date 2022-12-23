45f2351152de26f9704a8c4cb76f66eafa5cd5c1
Alejandro Arbiza <alejandro@roetal.com>
2022-12-22 21:56:41 -0500

    Enhance response tools
    	modified:   backend/private/fat/Helpers/ResponseTools.php

--- backend/private/fat/Helpers/ResponseTools.php
+++ backend/private/fat/Helpers/ResponseTools.php
@@ -5,6 +5,29 @@ namespace Fat\Helpers;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class ResponseTools {
+  static public function stop() {
+    die();
+  }
+
+  static public function withBody(Response $response, $data, $status = null) {
+    $clone = clone $response;
+    $clone->getBody()->write($data);
+    if (!is_null($status)) {
+      $clone = $clone->withStatus($status);
+    }
+    return $clone;
+  }
+
+  static public function withJson(Response $response, $data, $status = null) {
+    $clone = clone $response;
+    $clone->getBody()->write(json_encode($data));
+    if (!is_null($status)) {
+      $clone = $clone->withStatus($status);
+    }
+    $clone = $clone->withHeader('Content-Type', 'application/json');
+    return $clone;
+  }
+
   static public function withRedirect(Response $response, $url, $status = null) {
     $clone = clone $response;
     $clone = $clone->withHeader('Location', (string)$url);
