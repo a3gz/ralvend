999f56e5eaf2cf401c622fd40557b23b98cfb70b
Alejandro Arbiza <alejandro@roetal.com>
2022-12-23 19:56:25 -0500

    Add the actual not-found execption handler
    The configuration was pointing to a file that didn't exist.
    
    	new file:   backend/private/app/core/Controllers/NotFountExceptionHandler.php

---
+++ backend/private/app/core/Controllers/NotFountExceptionHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Core\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Psr7\Response;
+use Fat\Helpers\Path;
+use Plugins\demo\templates\DefaultTemplate;
+
+class NotFountExceptionHandler {
+  public function notFound(
+    ServerRequestInterface $request,
+    \Throwable $exception,
+    bool $displayErrorDetails
+  ) {
+    $response = new Response();
+    $tpl = new DefaultTemplate(Path::makePluginsPath('/demo'));
+    $tpl->define('content', 'views/components/404.php')
+      ->write($response);
+    return $response->withStatus(404);
+  }
+}
+
+// EOF
