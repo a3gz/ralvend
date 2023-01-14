129c82e2f1aa8e8dcac644c627e706ec9cfed0a6
Alejandro Arbiza <alejandro@roetal.com>
2023-01-14 08:32:30 -0500

    Parameterize OAuth2 access token TTL
    	modified:   backend/private/app/config/container/main.php

--- backend/private/app/config/container/main.php
+++ backend/private/app/config/container/main.php
@@ -8,6 +8,12 @@ return [
   'notFoundHandler' => function ($container) {
     return \App\Core\Controllers\NotFountExceptionHandler::class . ':notFound';
   },
+  // Time To Live for OAuth2 access tokens.
+  // Access tokens must be distributed to all the objects in the grid.
+  // If you don't set one here it will default to 10 Years. You need to evaluate
+  // your security needs and adjust it.
+  // https://www.php.net/manual/en/dateinterval.construct.php
+  'accessTokenTTL' => 'P10Y',
   // Take a look at the files inside private/app/lib/DataAdapters to know
   // more about this.
   // In particular, the method DataAdapterFactory::makeDataAdapter().
